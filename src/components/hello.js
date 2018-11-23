25fcc42398e061e04c5e96e934680f2646afb551
Bashir Shallah <bashir.shallah@gmail.com>
2018-11-23 19:00:57 +0300

    working with state.

--- src/components/hello.js
+++ src/components/hello.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 
 class Hello extends Component {
 
+    state = {
+        age: 15,
+        user: {
+            name: "bashir",
+            lastName: "shallah"
+        },
+        socialLinks: [
+            "facebook",
+            "twitter"
+        ]
+    }
+
     render(){
-        return <div>Hello {this.props.name} from class component.</div>
+        return <div>
+            Hello {this.props.name} from class component.
+            <p>{this.state.user.name} Age: {this.state.age}</p>
+        </div>
     }
 
 }
