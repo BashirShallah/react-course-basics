5f463b44a1caa417ba663038fbfb30366ea5be10
Bashir Shallah <bashir.shallah@gmail.com>
2018-11-23 18:44:41 +0300

    create a component.

---
+++ src/components/hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+
+    render(){
+        return <div>Hello from class component.</div>
+    }
+
+}
+
+export default Hello;
---
+++ src/components/welcome.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Test(){
+    return <div>welcome from function component.</div>;
+}
+
+export default Test;
