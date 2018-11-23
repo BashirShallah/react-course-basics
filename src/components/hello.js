48684a8de82b68de14b4f19dd1b6d390c76965cb
Bashir Shallah <bashir.shallah@gmail.com>
2018-11-23 19:11:46 +0300

    working with events.

--- src/components/hello.js
+++ src/components/hello.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 class Hello extends Component {
 
+    constructor(props){
+        super(props);
+
+        this.increase = this.increase.bind(this);
+    }
+
     state = {
         age: 15,
         user: {
@@ -14,10 +20,21 @@ class Hello extends Component {
         ]
     }
 
+    decrement = () => {
+        this.setState({age: this.state.age - 1});
+    }
+
+    increase(){
+        this.setState({age: this.state.age + 1});
+    }
+
     render(){
         return <div>
             Hello {this.props.name} from class component.
             <p>{this.state.user.name} Age: {this.state.age}</p>
+            <button onClick={()=>{this.setState({age: this.state.age + 1}) }}>Increase</button>
+            <button onClick={this.decrement}>Decrease</button>
+            <button onClick={this.increase}>Increase with function</button>
         </div>
     }
 
